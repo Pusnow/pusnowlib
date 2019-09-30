788c04850da1c991c0d97b970e4bddb82b7b1ad5
Wonsup Yoon <pusnow@kaist.ac.kr>
2019-09-30 14:44:46 +0900

    add dj_bus

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pusnowlib",
-    version="0.0.5",
+    version="0.0.6",
     author="Wonsup Yoon",
     author_email="pusnow@me.com",
     license="MIT License",
