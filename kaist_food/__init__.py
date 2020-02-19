3c02ffdad8d2dfa52f12e9a72a6a5b9312420774
Wonsup Yoon <pusnow@kaist.ac.kr>
2020-02-19 10:25:34 +0900

    new food page

--- kaist_food/__init__.py
+++ kaist_food/__init__.py
@@ -3,7 +3,7 @@ import re
 
 from aiohttp import ClientSession
 
-BASE_URL = "https://www.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=%s&site_dvs=mobile"
+BASE_URL = "https://www.kaist.ac.kr/kr/html/campus/053001.html?"
 DVS_DS = [
     ("fclt", "카이마루"),
     ("west", "서맛골"),
@@ -15,13 +15,11 @@ DVS_DS = [
     ("seoul", "서울캠퍼스"),
 ]
 
-TABLE = re.compile(
-    r"<table.*?class=\"menuTb\".*?>.*?<tbody>(.*?)</tbody>.*?</table>",
-    re.DOTALL)
 MENU = re.compile(r"<td.*?>(.*?)</td>", re.DOTALL)
 
 
 def format_menu(menu):
+    menu = re.sub(r"<!--.*?-->", "", menu, re.DOTALL)
     menu = re.sub(r"<.*?>", "", menu)
     menu = menu.replace("&lt;", "<").replace("&gt;", ">")
     menu = menu.replace("&amp;", "&").replace("&quot;", "\"")
@@ -29,24 +27,23 @@ def format_menu(menu):
     menu = menu.replace("미운영", "")
     menu = "\n".join((line.strip() for line in menu.splitlines()))
     menu = re.sub(r"\n\n\n*", "\n\n", menu)
+    menu = menu.strip()
     return menu
 
 
 async def fetch_ds(session, entry):
-    url = BASE_URL % entry[0]
-    async with session.get(url) as response:
+    async with session.post(BASE_URL, data={"dvs_cd": entry[0]}) as response:
         assert response.status == 200
         body = await response.text()
         menus = [format_menu(menu) for menu in MENU.findall(body)]
         if len(menus) != 3:
             return {
                 "ds": entry[1],
-                "url": url,
+                "url": BASE_URL,
             }
-
         return {
             "ds": entry[1],
-            "url": url,
+            "url": BASE_URL,
             "breakfast": menus[0],
             "lunch": menus[1],
             "dinner": menus[2],
@@ -75,7 +72,10 @@ if __name__ == "__main__":
     for food in get_foods():
         print(food["ds"])
         print(food["url"])
+        print("==== 아침 =====")
         print(food["breakfast"])
+        print("==== 점심 =====")
         print(food["lunch"])
+        print("==== 저녁 =====")
         print(food["dinner"])
         print("\n\n\n")
