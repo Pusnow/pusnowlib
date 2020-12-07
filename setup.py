4c325ddb4dbf80f3c9312bb88d726da8d080dc8e
Wonsup Yoon <pusnow@kaist.ac.kr>
2020-12-07 13:18:17 +0900

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pusnowlib",
-    version="0.0.9",
+    version="0.0.10",
     author="Wonsup Yoon",
     author_email="pusnow@me.com",
     license="MIT License",
